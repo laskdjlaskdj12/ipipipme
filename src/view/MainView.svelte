b12c432bee32aed24e9773ffde0a5c930b869c4a
laskdjlaskdj12 <laskdjlaskdj12@gmail.com>
2024-09-05 09:39:38 +0900

    AWS로 접속한 IP조회하는 기능 추가

--- src/view/MainView.svelte
+++ src/view/MainView.svelte
@@ -1,13 +1,52 @@
 <script>
+    import {beforeUpdate, onMount} from "svelte";
+
+    let ip_address = ""
+
+    const getIPFromAmazon = () => {
+        fetch("https://checkip.amazonaws.com/").then(res => res.text()).then(data => {
+            ip_address = data
+        })
+    }
+    onMount(() => {
+        getIPFromAmazon()
+    })
 
 </script>
 
 <main>
-    <h1>
-        Hello, World!
-    </h1>
+    <div id="center">
+        <h1 class="title nanum-gothic-bold">
+            Your IP IS
+        </h1>
+        <h3 class="subtitle nanum-gothic-bold">{ip_address}</h3>
+    </div>
 </main>
 
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&display=swap');
 
+    .nanum-gothic-bold {
+        font-family: "Nanum Gothic", system-ui;
+        font-weight: 700;
+        font-style: normal;
+    }
+
+    #center {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .title {
+        font-size: 40px;
+        font-weight: bold;
+    }
+
+    .subtitle {
+        font-size: 60px;
+        font-weight: bold;
+        margin-top: 10px
+    }
 </style>
